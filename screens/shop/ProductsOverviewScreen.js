3bde187637e1b0712eaaf1f95625d988315e1c78
Alan Pečenković <alanquad@yahoo.com>
2022-02-02 13:02:38 +0100

    making ProductItem component which is presentational and styling it

--- screens/shop/ProductsOverviewScreen.js
+++ screens/shop/ProductsOverviewScreen.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { View, Text, StyleSheet, FlatList, Button } from "react-native";
 import { useSelector } from "react-redux";
+import ProductItem from "../../components/shop/ProductItem";
 
 const ProductsOverviewScreen = (props) => {
   const products = useSelector((state) => state.products.availableProducts);
@@ -8,7 +9,15 @@ const ProductsOverviewScreen = (props) => {
     <View style={styles.screen}>
       <FlatList
         data={products}
-        renderItem={(itemData) => <Text>{itemData.item.title}</Text>}
+        renderItem={(itemData) => (
+          <ProductItem
+            image={itemData.item.imageUrl}
+            title={itemData.item.title}
+            price={itemData.item.price}
+            onViewDetail={() => console.log("Detail View")}
+            onAddToCart={() => console.log("Added to Cart")}
+          />
+        )}
       />
       <Button
         title="Product Detail"
