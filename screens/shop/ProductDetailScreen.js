de31f2695306f228ca688c475e58069a4585434c
Alan Pečenković <alanquad@yahoo.com>
2022-02-02 11:25:02 +0100

    Added button in ProductDetail which navigates to itself with navigate.push

--- screens/shop/ProductDetailScreen.js
+++ screens/shop/ProductDetailScreen.js
@@ -1,10 +1,14 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, Button } from "react-native";
 
-const ProductDetailScreen = () => {
+const ProductDetailScreen = ({ navigation }) => {
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
       <Text>Details Screen</Text>
+      <Button
+        title="Go to Details... again"
+        onPress={() => navigation.push("ProductDetails")}
+      />
     </View>
   );
 };
