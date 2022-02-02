a87cf86efe8f134b3f964d8ac6c3c75f717c81df
Alan Pečenković <alanquad@yahoo.com>
2022-02-02 11:10:26 +0100

    Navigating to a new screen with navigation which is passed as a prop

--- screens/shop/ProductsOverviewScreen.js
+++ screens/shop/ProductsOverviewScreen.js
@@ -1,15 +1,29 @@
 import React from "react";
-import { View, Text, StyleSheet, FlatList } from "react-native";
+import { View, Text, StyleSheet, FlatList, Button } from "react-native";
 import { useSelector } from "react-redux";
 
 const ProductsOverviewScreen = (props) => {
   const products = useSelector((state) => state.products.availableProducts);
   return (
-    <FlatList
-      data={products}
-      renderItem={(itemData) => <Text>{itemData.item.title}</Text>}
-    />
+    <View style={styles.screen}>
+      <FlatList
+        data={products}
+        renderItem={(itemData) => <Text>{itemData.item.title}</Text>}
+      />
+      <Button
+        title="Product Detail"
+        onPress={() => props.navigation.navigate("ProductDetails")}
+      />
+    </View>
   );
 };
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
 
 export default ProductsOverviewScreen;
