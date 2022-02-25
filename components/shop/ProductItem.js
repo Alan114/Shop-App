b50f2c4021455b6a579e80e6484f68a163c4cd1f
Alan Pečenković <alanquad@yahoo.com>
2022-02-25 11:23:24 +0100

    Adding a Card component which is reused in other components to not repeat code

--- components/shop/ProductItem.js
+++ components/shop/ProductItem.js
@@ -8,6 +8,7 @@ import {
   TouchableNativeFeedback,
   Platform,
 } from "react-native";
+import Card from "../UI/Card";
 
 const ProductItem = (props) => {
   let TouchableComponent = TouchableOpacity;
@@ -15,7 +16,7 @@ const ProductItem = (props) => {
     TouchableComponent = TouchableNativeFeedback;
   }
   return (
-    <View style={styles.product}>
+    <Card style={styles.product}>
       <TouchableComponent onPress={props.onSelect} useForeground>
         <View>
           <View style={styles.imageContainer}>
@@ -28,19 +29,12 @@ const ProductItem = (props) => {
           <View style={styles.actions}>{props.children}</View>
         </View>
       </TouchableComponent>
-    </View>
+    </Card>
   );
 };
 
 const styles = StyleSheet.create({
   product: {
-    shadowColor: "black",
-    shadowOpacity: 0.26,
-    shadowOffset: { width: 0, height: 2 },
-    shadowRadius: 8,
-    elevation: 5,
-    borderRadius: 10,
-    backgroundColor: "white",
     height: 330,
     width: 330,
     margin: 20,
