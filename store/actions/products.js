336ef79190d6a200cdc3f26fbcfe3a02a7231c7f
Alan Pečenković <alanquad@yahoo.com>
2022-03-09 11:31:41 +0100

    Making a PATCH request to firebase to update existing data

--- store/actions/products.js
+++ store/actions/products.js
@@ -79,13 +79,30 @@ export const createProduct = (title, description, imageUrl, price) => {
   };
 };
 export const updateProduct = (id, title, description, imageUrl) => {
-  return {
-    type: UPDATE_PRODUCT,
-    pid: id,
-    productData: {
-      title,
-      description,
-      imageUrl,
-    },
+  return async (dispatch) => {
+    await fetch(
+      `https://rn-complete-guide-9e68f-default-rtdb.europe-west1.firebasedatabase.app/products/${id}.json`,
+      {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          title,
+          description,
+          imageUrl,
+        }),
+      }
+    );
+
+    dispatch({
+      type: UPDATE_PRODUCT,
+      pid: id,
+      productData: {
+        title,
+        description,
+        imageUrl,
+      },
+    });
   };
 };
